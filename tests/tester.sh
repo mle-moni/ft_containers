85de087d08a0770241bf69b17fac0b820275379f
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2020-09-12 17:30:07 +0200

    no warnings when starting script from a non tty interface

--- tests/tester.sh
+++ tests/tester.sh
@@ -1,8 +1,11 @@
-export NORMAL=$(tput sgr0)
-export RED=$(tput setaf 1)
-export GREEN=$(tput setaf 2)
-export YELLOW=$(tput setaf 3)
-export BLUE=$(tput setaf 4)
+if tty -s
+then
+	export NORMAL=$(tput sgr0)
+	export RED=$(tput setaf 1)
+	export GREEN=$(tput setaf 2)
+	export YELLOW=$(tput setaf 3)
+	export BLUE=$(tput setaf 4)
+fi
 
 ERROR="0"
 
